b92d8206b1ae480312cdf7dd46a5500f8de778b1
Spencer Brown <spencerb@warpmail.net>
2018-11-16 16:58:42 -0500

    Add graphics module

---
+++ graphics.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_pixels.h>
+
+typedef struct {
+    
+} RenderTarget;
+
+#endif // GRAPHICS_H
--- main.c
+++ main.c
@@ -1,84 +1,17 @@
-#include <ncurses.h>
+#include "graphics.h"
+
 #include <stdbool.h>
-#include <assert.h>
 
-#define ESC 27
-#define UP 'w'
-#define DOWN 's'
-#define WIDTH 10
-#define HEIGHT LINES
-#define X 0
-#define Y 0
+int mainLoop();
 
-typedef enum EVENT {
-    EVENT_UNKNOWN = -1,
-    EVENT_ESC = 0,
-    EVENT_UP = 1,
-    EVENT_DOWN = 2
-} EVENT;
-
-EVENT get_input();
-void update_game_data(EVENT event);
-void render_game(WINDOW* window);
-WINDOW * init_game();
-void cleanup();
-
-int main()
+int main(int argc, char argv[][])
 {
-    WINDOW *game_window = init_game();
-
-    bool game_is_still_running = true;
-    while (game_is_still_running) {
-        EVENT event = get_input();
-        if (event == EVENT_ESC) {
-            game_is_still_running = false;
-            continue;
-        }
-
-        update_game_data(event);
-
-        render_game(game_window);
-    }
-
-    cleanup();
+    return 0;
 }
 
-void render_game(WINDOW* window)
+int mainLoop()
 {
-    move(5, 5);
-    waddch(window, '$');
-    wrefresh(window);
+    // get and process user input
+    // process game update (ticks)
+    renderGame();
 }
-
-void update_game_data(EVENT event)
-{
-}
-
-EVENT get_input()
-{
-    int input_value = getch();
-    if (input_value == ESC) {
-        return EVENT_ESC;
-    } else if (UP) { // up key
-        return EVENT_UP;
-    } else if (DOWN) { //down key
-        return EVENT_DOWN;
-    } else {
-        return EVENT_UNKNOWN;
-    }
-}
-
-WINDOW *init_game()
-{
-    initscr();
-    cbreak();
-    noecho();
-    WINDOW * win = newwin(HEIGHT, WIDTH, Y, X);
-    return win;
-}
-
-void cleanup()
-{
-    endwin();
-}
-
